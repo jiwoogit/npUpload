80a467699005ec2926e5e9296052610e75ce6c89
Your Name <you@example.com>
2021-04-09 17:47:36 +0900

    tflow file add

--- asm1/awk.sh
+++ asm1/awk.sh
@@ -1,6 +1,6 @@
 awk '$1=="1" {print $2 "\t" $3}' logudp.dat > flow1.dat;
 awk '$1=="2" {print $2 "\t" $3}' logudp.dat > flow2.dat;
 awk '$1=="3" {print $2 "\t" $3}' logudp.dat > flow3.dat;
-awk '$1=="1" {print $2 "\t" $3}' logtcp.dat > flow1.dat;
-awk '$1=="2" {print $2 "\t" $3}' logtcp.dat > flow2.dat;
-awk '$1=="3" {print $2 "\t" $3}' logtcp.dat > flow3.dat;
+awk '$1=="1" {print $2 "\t" $3}' logtcp.dat > tlow1.dat;
+awk '$1=="2" {print $2 "\t" $3}' logtcp.dat > tlow2.dat;
+awk '$1=="3" {print $2 "\t" $3}' logtcp.dat > tlow3.dat;
