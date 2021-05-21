541da8ee0f8bc1c8a9cd66f9cff3be8c3c8a1c11
Your Name <you@example.com>
2021-05-21 15:46:59 +0900

    loss add

--- assn3/q3.sh
+++ assn3/q3.sh
@@ -6,4 +6,6 @@ awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log3.out > ~/gitUpload/as
 awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log4.out > ~/gitUpload/assn3/q34.out
 awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log5.out > ~/gitUpload/assn3/q35.out
 awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log6.out > ~/gitUpload/assn3/q4.out
+awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log7.out > ~/gitUpload/assn3/q5.out
+awk '$1=="FrameConsumerLog::RemainFrames:" {print $2}' log8.out > ~/gitUpload/assn3/q6.out
 
